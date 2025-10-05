ddbbc5c6e6568b8200bd943c7f9a06194aa06e2f
aran8276 <aran8276@gmail.com>
2025-10-05 16:40:37 +0700

    python debug

--- scripts/appveyor-win.sh
+++ scripts/appveyor-win.sh
@@ -6,6 +6,8 @@ BUILD_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 BUILD_ROOT="$(echo $BUILD_ROOT | sed 's|^/\([a-z]\)|\1:|g')" # replace /c/... by c:/... for cmake to understand it
 echo "BUILD_ROOT=$BUILD_ROOT"
 
+/c/Python37/python.exe -m pip install protobuf==3.19.5
+
 mkdir ninja && cd ninja
 wget https://github.com/ninja-build/ninja/releases/download/v1.9.0/ninja-win.zip && 7z x ninja-win.zip
 cd ..
